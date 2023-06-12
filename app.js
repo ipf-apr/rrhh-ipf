2a2e89cdef49bc081193583c04d28bdf51d4c508
alexsh882 <alexsh882@gmail.com>
2023-06-12 12:51:50 -0300

    Se agregó al package.jon las dependencias cors, helmet, morgan y en  dev sequelize-cli

--- app.js
+++ app.js
@@ -1,45 +1,55 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const path = require('path');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const { isAuthenticated } = require("./controllers/auth.controllers");
 
 const app = express();
 
 //configuración del motor de platillas
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //Carpeta public para archivos estaticos
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //procesamiento de datos
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'public')));
+app.use(cors());
+app.use(
+  helmet({
+    contentSecurityPolicy : false
+  })
+);
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 
 //variables de entorno
-dotenv.config({path:'.env'})
+dotenv.config({ path: ".env" });
 
 //trabajar con las cookies
 app.use(cookieParser());
 
 //llamar al router
-app.use('/', require('./routes/auth.routes'))
-app.use('/', require('./routes/employee.routes'))
+app.use("/", require("./routes/auth.routes"));
+app.use("/employees", isAuthenticated, require("./routes/employee.routes"));
 
 //eliminar la cache para que no se pueda volver atras
-app.use(function(req,res,next){
-    if(!req.username){
-        res.header('Cache-Control','private, no-cache, no-store, must-revalidate');
-        next();
-    }
+app.use(function (req, res, next) {
+  if (!req.username) {
+    res.header("Cache-Control", "private, no-cache, no-store, must-revalidate");
+    next();
+  }
 
-    res.status(404).render('errors/404.ejs');
-})
-
+  res.status(404).render("errors/404.ejs");
+});
 
 //poner el marcha el server
-app.listen(process.env.APP_PORT, ()=>{
-    console.log(`Servidor en ${process.env.APP_URL}:${process.env.APP_PORT}`) 
-})
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Servidor en ${process.env.APP_URL}:${process.env.APP_PORT}`);
+});
