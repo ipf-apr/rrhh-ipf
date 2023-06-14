614c6b6e28b7313dfed1b2dd439620860ee075eb
Alejandro <alexsh882@gmail.com>
2023-06-13 22:40:15 -0300

    Se instaló el paquete method-override para manejar las meticiones PUT y DELETE

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const { isAuthenticated } = require("./controllers/auth.controllers");
 
 const app = express();
 
+var methodOverride = require('method-override')
+
 //configuración del motor de platillas
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -20,6 +22,7 @@ app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'))
 app.use(cors());
 app.use(
   helmet({
