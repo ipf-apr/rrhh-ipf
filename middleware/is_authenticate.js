190b3f54a5e9fa5f950a4fc3d29ee6b6c4c81a73
Alejandro <alexsh882@gmail.com>
2023-06-20 22:42:22 -0300

    Se agregó un "handler" dentro de is_authenticate para el manejo de token expirados.

--- middleware/is_authenticate.js
+++ middleware/is_authenticate.js
@@ -2,29 +2,38 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 const isAuthenticated = async (req, res, next) => {
-  // Leer el token
+    // Leer el token
 
-  let token = req.header('Authorization');
+    let token = req.header('Authorization');
 
-  //esta parte se agregó para las páginas, ya que no se puede agregar el header a la petición GET
-  if (!token) {
-      token = req.cookies.jwt;  
-      console.log('req.cookies.jwt');  
-  }
+    //esta parte se agregó para las páginas, ya que no se puede agregar el header a la petición GET
+    if (!token) {
+        token = req.cookies.jwt;
+    }
 
- 
-  
-  if (!token) {
-    return res.status(401).json({
-      message: 'No hay token en la petición',
-    });
-  }
 
-  try {
-        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (!token) {
+        // return res.status(401).json({
+        //   message: 'No hay token en la petición',
+        // });
+
+        return res.redirect("/login");
+    }
+
+    try {
+
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET, function (error, decoded) {
+            if (error) {
+                res.clearCookie("jwt");
+                return res.redirect("/login");
+            }
+            return decoded;
+        });
 
         // Leer el usuario que corresponde al id
-        const user = await User.findByPk(id);
+        const user = await User.findByPk(decoded.id);
 
         if (!user) {
             return res.status(401).json({
@@ -43,9 +52,10 @@ const isAuthenticated = async (req, res, next) => {
 
         next();
     } catch (error) {
+        console.log('catch (error)');
         console.log(error);
         return res.status(401).json({
-            message: 'Token no válido',
+            message: 'Token inválido',
         });
     }
 };
