30b4bda93b63a829605bb446fce201a633a89c04
alexsh882 <alexsh882@gmail.com>
2023-10-11 21:27:57 -0300

    chores: cambios menores en textos.

--- public/js/jobPositions/storeUpdateModal.js
+++ public/js/jobPositions/storeUpdateModal.js
@@ -1,119 +1,120 @@
-const createEditJobPositionForm = document.querySelector("#createEditJobPositionForm");
+const createEditJobPositionForm = document.querySelector(
+  "#createEditJobPositionForm"
+);
 
 const positionForm = document.querySelector("#position");
 const permanencyForm = document.querySelector("#permanency");
 const isCreating = document.querySelector("#isCreating");
-const myModal = new bootstrap.Modal(document.querySelector('#modalJobPositionCreate'));
+const myModal = new bootstrap.Modal(
+  document.querySelector("#modalJobPositionCreate")
+);
 
 const createJobPosition = async (event) => {
+  myModal.show();
+};
 
-    myModal.show();
-  
-  }
-  
-  const editJobPosition = async (event) => {
-    const { id, position } = event.target.dataset;
-    positionForm.value = position;
-    isCreating.value = id;
-    myModal.show();
-  
-  }
-  
-  const deleteJobPosition = async (event) => {
-    const id = event.target.dataset.id;
-  
-    Swal.fire({
-      title: "Estás seguro?",
-      text: `Estás por eliminar un puesto laboral del sistema!`,
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Estoy seguro!",
-      cancelButtonText: "Cancelar",
-    }).then(async (result) => {
-      if (result.isConfirmed) {
-        try {
-          const res = await fetch(
-            `http://localhost:8000/api/job-positions/${id}/destroy`,
-            {
-              method: "DELETE",
-            }
-          );
-  
-          const data = await res.json();
-  
-          Swal.fire({
-            icon: "success",
-            title: "Categoría eliminada",
-            text: data.message,
-          });
-  
-          setTimeout(() => {
-            window.location.reload();
-          }, 2200);
-        } catch (error) {
-          console.log(error);
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: error.message,
-          });
-        }
+const editJobPosition = async (event) => {
+  const { id, position } = event.target.dataset;
+  positionForm.value = position;
+  isCreating.value = id;
+  myModal.show();
+};
+
+const deleteJobPosition = async (event) => {
+  const id = event.target.dataset.id;
+
+  Swal.fire({
+    title: "Estás seguro?",
+    text: `Estás por eliminar un puesto laboral del sistema!`,
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Estoy seguro!",
+    cancelButtonText: "Cancelar",
+  }).then(async (result) => {
+    if (result.isConfirmed) {
+      try {
+        const res = await fetch(
+          `http://localhost:8000/api/job-positions/${id}/destroy`,
+          {
+            method: "DELETE",
+          }
+        );
+
+        const data = await res.json();
+
+        Swal.fire({
+          icon: "success",
+          title: "Puesto Laboral eliminado",
+          text: data.message,
+        });
+
+        setTimeout(() => {
+          window.location.reload();
+        }, 2200);
+      } catch (error) {
+        console.log(error);
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: error.message,
+        });
       }
-    });
+    }
+  });
+};
+
+createEditJobPositionForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const formData = {
+    position: positionForm.value,
   };
-  
-  createEditJobPositionForm.addEventListener('submit', async (e) => {
-    e.preventDefault();
-  
-  
-    const formData = {
-      position: positionForm.value,
-    }
-  
-    let url, method;
-  
-    if (!isCreating.value) {
-      url = 'http://localhost:8000/api/job-positions'
-      method = 'POST'
-    } else {
-      url = `http://localhost:8000/api/job-positions/${isCreating.value}/update`
-      method = 'PUT'
-    }
-  
-    const response = await fetch(url, {
-      method,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData),
-    });
-  
-    const respToJson = await response.json();
-  
-    console.log(respToJson);
-  
-    if (response.status !== 201 && response.status !== 200) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Oops...',
-        text: respToJson.message,
-      });
-      return;
-    }
-  
+
+  let url, method;
+
+  if (!isCreating.value) {
+    url = "http://localhost:8000/api/job-positions";
+    method = "POST";
+  } else {
+    url = `http://localhost:8000/api/job-positions/${isCreating.value}/update`;
+    method = "PUT";
+  }
+
+  const response = await fetch(url, {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(formData),
+  });
+
+  const respToJson = await response.json();
+
+  console.log(respToJson);
+
+  if (response.status !== 201 && response.status !== 200) {
     Swal.fire({
-      icon: 'success',
-      title: 'Muy Bien!',
+      icon: "error",
+      title: "Oops...",
       text: respToJson.message,
     });
-  
-    myModal.hide();
-    createEditJobPositionForm.reset();
-  
-    if (!respToJson.jobPosition.id) {
-      categoriesList.innerHTML += `
+    return;
+  }
+
+  Swal.fire({
+    icon: "success",
+    title: "Muy Bien!",
+    text: respToJson.message,
+  });
+
+  myModal.hide();
+  createEditJobPositionForm.reset();
+
+
+  if (!isCreating.value) {
+    jobPositionsList.innerHTML += `
         <tr>
             <th scope="row">
               ${respToJson.jobPosition.id}
@@ -127,11 +128,9 @@ const createJobPosition = async (event) => {
             </td>
         </tr>
       `;
-    } else {
-      setTimeout(() => {
-        window.location.reload();
-      }, 2200);
-    }
-  
-  
-  })
+  } else {
+    setTimeout(() => {
+      window.location.reload();
+    }, 2200);
+  }
+});
