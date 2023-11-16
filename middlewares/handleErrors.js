65b1edd978370b4921ac77fccb908d8f668c4dea
alexsh_home <alexsh882@gmail.com>
2023-11-16 20:24:54 -0300

    refactor: se mejoró el error handler.

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,15 +1,14 @@
 const path = require('path')
 const storeLogs = require('../helpers/storeLogs')
 
-
+//function to handle errors in the app
 const handleErrors = (err, _req, res, next) => {
-    
-    console.log(err);
+
     const date = new Date();
-    const timestamp = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+    const timestamp = `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
     storeLogs(`${timestamp} - ${err.stack}\n`, path.dirname(__dirname), 'logs/errors');
     const errorMessage = JSON.parse(err);
-    res.status(errorMessage.status).send(errorMessage.Error);
+    res.status(errorMessage.status).send(errorMessage.Error || errorMessage.error);
 };
 
 module.exports = handleErrors;
