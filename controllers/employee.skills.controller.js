5c6eb574d02cbd27ff23086720b2008776c727a2
alexsh_home <alexsh882@gmail.com>
2023-11-26 17:21:11 -0300

    refactor: del controlador de empleado habilidad, además que ahora devuelve si si agregó la habilidad al empleado.

--- controllers/employee.skills.controller.js
+++ controllers/employee.skills.controller.js
@@ -3,70 +3,70 @@ const Employee = require('../models/employee');
 const EmployeeSkill = require('../models/employeeSkill');
 
 const index = async (req, res) => {
-    const { employeeId } = req.params;
-  
-    try {
-      const employee = await Employee.findByPk(employeeId, {
-        include: [
-          {
-            model: Skill,
-            as: 'employeeSkills', 
-            through: {
-              attributes: [] 
-            }
+  const { employeeId } = req.params;
+
+  try {
+    const employee = await Employee.findByPk(employeeId, {
+      include: [
+        {
+          model: Skill,
+          as: 'employeeSkills',
+          through: {
+            attributes: []
           }
-        ]
-      });
-  
-      if (!employee) {
-        throw {
-          status: 404,
-          message: 'El empleado no existe',
-        };
-      }
-  
-      return res.json({
-        data: employee.employeeSkills, 
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: 'Error interno del servidor' });
-    }
-  };
-  
-const store = async (req,res)=>{
-    const {employeeId, skillId} = req.params;
-    try {
-
-      const [categoryEmployee, created] = await EmployeeSkill.findOrCreate({
-        where: {
-          EmployeeId: employeeId,
-          SkillId: skillId,
         }
-      });
-  
-      if (!created) {
-        return res.status(200).json({
-          message: "La habilidad ya fue agregada a este empleado.",
-        });
-      }
-      if (!categoryEmployee) {
-        throw {
-          status: 400,
-          message: "No se pudo relacionar la habilidad al empleado",
-        };
-      }
-  
-      return res.status(201).json({
-        message: "Habilidad agregada al empleado correctamente",
-      });
+      ]
+    });
 
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({message:'Error interno del servidor!'})
+    if (!employee) {
+      throw {
+        status: 404,
+        message: 'El empleado no existe',
+      };
     }
+
+    return res.json(employee.employeeSkills
+    );
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Error interno del servidor' });
+  }
+};
+
+const store = async (req, res) => {
+  const { employeeId, skillId } = req.params;
+  try {
+
+    const [categoryEmployee, created] = await EmployeeSkill.findOrCreate({
+      where: {
+        EmployeeId: employeeId,
+        SkillId: skillId,
+      }
+    });
+
+    if (!created) {
+      return res.status(200).json({
+        message: "La habilidad ya fue agregada a este empleado.",
+      });
+    }
+    if (!categoryEmployee) {
+      throw {
+        status: 400,
+        message: "No se pudo relacionar la habilidad al empleado",
+      };
+    }
+
+    return res.status(201).json({
+      categoryEmployee,
+      message: "Habilidad agregada al empleado correctamente",
+    });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: 'Error interno del servidor!' })
+  }
 };
 module.exports = {
-    store,
-    index
+  store,
+  index
 }
